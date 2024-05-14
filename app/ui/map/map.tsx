b9d069e62b88d514254fff6c56434c8e0c417365
dzzirima <dzzirima@gmail.com>
2024-05-14 15:55:20 +0200

    after merge to map

--- app/ui/map/map.tsx
+++ app/ui/map/map.tsx
@@ -49,7 +49,6 @@ const Map = () => {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-
                 {/* <Marker icon={
                     new L.Icon({
                         iconUrl: MarkerIcon.src,
